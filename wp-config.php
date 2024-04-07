8301fe5c1cc69cd0da0c529a8e258679bbfab33a
Wibisono Priyo Sembodo <92106028+Wibisono1284@users.noreply.github.com>
2024-04-07 18:05:10 +0700

    Add files via upload

---
+++ wp-config.php
@@ -0,0 +1,6 @@
+define( 'WP_REDIS_HOST', 'redis-server' );
+define( 'WP_REDIS_PORT', 6379 );
+define( 'WP_REDIS_PREFIX', 'dolanan' );
+define( 'WP_REDIS_DATABASE', 0 ); // 0-15
+define( 'WP_REDIS_TIMEOUT', 1 );
+define( 'WP_REDIS_READ_TIMEOUT', 1 );
